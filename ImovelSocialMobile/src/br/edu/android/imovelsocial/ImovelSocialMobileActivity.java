55f0cc1e3a22f01ed534e57122edf18f1e24f03a
Jorge <jorgedanielbarrosjr@hotmail.com>
2011-11-19 19:45:37 -0200

    Versão Android do aplicativo Imóvel Social para dispositivos móveis.

---
+++ ImovelSocialMobile/src/br/edu/android/imovelsocial/ImovelSocialMobileActivity.java
@@ -0,0 +1,13 @@
+package br.edu.android.imovelsocial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImovelSocialMobileActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
